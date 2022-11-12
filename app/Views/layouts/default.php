c22f44ddd24ecff0bcf1049578794df8633ca076
Guilherme Daniel <gui.dani13@gmail.com>
2022-11-12 15:36:43 -0300

    initialized home page

---
+++ app/Views/layouts/default.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Um crud simples usando Codeigniter 4">
+  <meta name="author" content="github.com/guidani">
+  <title>Service Orders CRUD</title>
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <?= $this->renderSection('content') ?>
+
+</body>
+
+</html>
